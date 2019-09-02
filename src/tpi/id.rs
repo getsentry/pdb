9bde94a3c88b276f1414ef743ecb181464f1cff9
Jan Michael Auer <mail@jauer.org>
2019-09-02 21:11:00 +0200

    Fix parsing of ID types

--- src/tpi/id.rs
+++ src/tpi/id.rs
@@ -3,16 +3,6 @@ use scroll::ctx::TryFromCtx;
 use crate::common::*;
 use crate::tpi::constants::*;
 
-#[inline]
-fn parse_optional_type_index<'t>(buf: &mut ParseBuffer<'t>) -> Result<Option<TypeIndex>> {
-    let index = buf.parse()?;
-    if index == TypeIndex(0) || index == TypeIndex(0xffff) {
-        Ok(None)
-    } else {
-        Ok(Some(index))
-    }
-}
-
 // TODO(ja): MOve these
 #[inline]
 fn parse_optional_id_index<'t>(buf: &mut ParseBuffer<'t>) -> Result<Option<IdIndex>> {
@@ -34,12 +24,12 @@ fn parse_string<'t>(leaf: u16, buf: &mut ParseBuffer<'t>) -> Result<RawString<'t
 /// Encapsulates parsed data about an `Id`.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum IdData<'t> {
-    FunctionId(FunctionIdType<'t>),
-    MemberFunctionId(MemberFunctionIdType<'t>),
-    BuildInfoId(BuildInfoIdType),
-    SubstringList(SubstringListType),
-    StringId(StringIdType<'t>),
-    UserDefinedTypeSource(UserDefinedTypeSourceType),
+    Function(FunctionId<'t>),
+    MemberFunction(MemberFunctionId<'t>),
+    BuildInfo(BuildInfoId),
+    StringList(StringListId),
+    String(StringId<'t>),
+    UserDefinedTypeSource(UserDefinedTypeSourceId),
 }
 
 impl<'t> IdData<'t> {}
@@ -53,38 +43,38 @@ impl<'t> TryFromCtx<'t, scroll::Endian> for IdData<'t> {
         let leaf = buf.parse_u16()?;
 
         let data = match leaf {
-            LF_FUNC_ID => IdData::FunctionId(FunctionIdType {
+            LF_FUNC_ID => IdData::Function(FunctionId {
                 scope: parse_optional_id_index(&mut buf)?,
                 function_type: buf.parse()?,
                 name: parse_string(leaf, &mut buf)?,
             }),
-            LF_MFUNC_ID => IdData::MemberFunctionId(MemberFunctionIdType {
-                parent: parse_optional_type_index(&mut buf)?,
+            LF_MFUNC_ID => IdData::MemberFunction(MemberFunctionId {
+                parent: buf.parse()?,
                 function_type: buf.parse()?,
                 name: parse_string(leaf, &mut buf)?,
             }),
-            LF_BUILDINFO => IdData::BuildInfoId({
+            LF_BUILDINFO => IdData::BuildInfo({
                 let count = buf.parse::<u16>()?;
                 let mut arguments = Vec::with_capacity(count as usize);
                 for _ in 0..count {
                     arguments.push(buf.parse()?);
                 }
-                BuildInfoIdType { arguments }
+                BuildInfoId { arguments }
             }),
-            LF_SUBSTR_LIST => IdData::SubstringList({
+            LF_SUBSTR_LIST => IdData::StringList({
                 let count = buf.parse::<u32>()?;
                 let mut substrings = Vec::with_capacity(count as usize);
                 for _ in 0..count {
                     substrings.push(buf.parse()?);
                 }
-                SubstringListType { substrings }
+                StringListId { substrings }
             }),
-            LF_STRING_ID => IdData::StringId(StringIdType {
-                substrings: buf.parse()?,
+            LF_STRING_ID => IdData::String(StringId {
+                substrings: parse_optional_id_index(&mut buf)?,
                 name: parse_string(leaf, &mut buf)?,
             }),
             LF_UDT_SRC_LINE | LF_UDT_MOD_SRC_LINE => {
-                let mut udt = UserDefinedTypeSourceType {
+                let mut udt = UserDefinedTypeSourceId {
                     udt: buf.parse()?,
                     source_file: buf.parse()?,
                     line: buf.parse()?,
@@ -105,54 +95,55 @@ impl<'t> TryFromCtx<'t, scroll::Endian> for IdData<'t> {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct FunctionIdType<'t> {
+pub struct FunctionId<'t> {
     /// Parent scope of this id.
-    scope: Option<IdIndex>,
+    pub scope: Option<IdIndex>,
     /// Index of the function type declaration.
-    function_type: TypeIndex,
-    name: RawString<'t>,
+    pub function_type: TypeIndex,
+    /// Name of the function.
+    pub name: RawString<'t>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct MemberFunctionIdType<'t> {
+pub struct MemberFunctionId<'t> {
     /// Index of the parent type.
-    parent: Option<TypeIndex>,
+    pub parent: TypeIndex,
     /// Index of the member function type declaration.
-    function_type: TypeIndex,
+    pub function_type: TypeIndex,
     /// Name of the member function.
-    name: RawString<'t>,
+    pub name: RawString<'t>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct BuildInfoIdType {
+pub struct BuildInfoId {
     /// Indexes of build arguments.
-    arguments: Vec<IdIndex>,
+    pub arguments: Vec<IdIndex>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct SubstringListType {
+pub struct StringListId {
     /// The list of substrings.
     pub substrings: Vec<TypeIndex>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct StringIdType<'t> {
+pub struct StringId<'t> {
     /// Index of the list of substrings.
-    substrings: IdIndex,
+    pub substrings: Option<IdIndex>,
     /// The string.
-    name: RawString<'t>,
+    pub name: RawString<'t>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct UserDefinedTypeSourceType {
+pub struct UserDefinedTypeSourceId {
     /// Index of the UDT's type definition.
-    udt: TypeIndex,
+    pub udt: TypeIndex,
     /// Index of the source file name.
-    source_file: IdIndex,
+    pub source_file: IdIndex,
     /// Line number in the source file.
-    line: u32,
+    pub line: u32,
     /// Module that contributes this UDT definition.
     ///
     /// If None, the UDT is declared in the same module.
-    module: Option<u16>,
+    pub module: Option<u16>,
 }
